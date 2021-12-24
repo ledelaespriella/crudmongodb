6a7e2cb8bcc4b4f4dcaf4239d1260b9bc97effd9
ledelaespriella <luisdelaespriellaj@hotmail.com>
2021-12-24 11:11:55 -0500

    se actualiza ruta de usuario

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
-import { register, login, getUser } from '../controller/user.controller';
+import {
+  register,
+  login,
+  getUser
+} from '../controller/user.controller';
 import { authMiddleware } from './../middleware/auth.middleware';
 
 const app = Router();
 
 app.post('/auth/login', login);
 app.post('/auth/register', register);
-app.get('/auth', getUser);
+app.get('/auth', authMiddleware , getUser);
 
 export default app;
