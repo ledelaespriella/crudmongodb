bc8c74e81e7b9b9e629660f8c90d73f90d19c0b0
ledelaespriella <luisdelaespriellaj@hotmail.com>
2021-12-25 11:42:16 -0500

    correcion de las rutas de categoria

--- src/router/cantegory.router.js
+++ src/router/cantegory.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   index,
+  indexLot,
   save,
   edit,
   update,
@@ -12,6 +13,7 @@ import { authMiddleware } from './../middleware/auth.middleware';
 const app = Router();
 
 app.get('/category', authMiddleware, index);
+app.get('/lot',authMiddleware, indexLot)
 app.post('/category', authMiddleware, save);
 app.get('/category/:categoryId', authMiddleware, edit);
 app.put('/category/:categoryId', authMiddleware, update);
